c73ac331128f0d73cc82e79f2b2bd3ebdc475a2a
Faheem Daruwala <faheemdaruwala1@gmail.com>
2025-12-25 12:24:12 +0530

    feat: update CORS origin and change server port; enhance MongoDB connection settings

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const app = express();
 
 // --------------------- Middlewares --------------------- //
 app.use(cors({
-  origin : 'http://localhost:3000',
+  origin : 'www.vms.nexcorealliance.com' || "https:vms.nexcorealliance.com",
   credentials : true,
 }));
 app.use(express.json());
@@ -61,7 +61,7 @@ app.use((err, req, res, next) => {
 });
 
 // --------------------- Start Server --------------------- //
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6094;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error("MongoDB connection error:", err.message);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`✅ MongoDB Atlas Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
